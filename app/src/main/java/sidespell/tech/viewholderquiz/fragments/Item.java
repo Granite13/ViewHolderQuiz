f7532757e36197154e4ade9f6a85610b31eced5e
Granite13 <whyaguilar@bscs.usjr.edu.ph>
2016-01-14 18:54:33 +0800

    added drawables

---
+++ app/src/main/java/sidespell/tech/viewholderquiz/fragments/Item.java
@@ -0,0 +1,22 @@
+package sidespell.tech.viewholderquiz.fragments;
+
+/**
+ * Created by whyaguilar on 1/14/16.
+ */
+public class Item {
+
+    public int Id;
+    public String IconFile;
+    public String AnimeName;
+    public String DescMov;
+
+    public Item(int id, String iconFile, String animeName, String descMov) {
+
+        Id = id;
+        IconFile = iconFile;
+        AnimeName = animeName;
+        DescMov = descMov;
+
+
+    }
+}
