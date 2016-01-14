10ddc7d9c98efe5f62b928b5abe0df0e0a3e84aa
Granite13 <whyaguilar@bscs.usjr.edu.ph>
2016-01-14 20:02:43 +0800

    added drawables

--- app/src/main/java/sidespell/tech/viewholderquiz/fragments/Item.java
+++ app/src/main/java/sidespell/tech/viewholderquiz/fragments/Item.java
@@ -1,22 +1,20 @@
 package sidespell.tech.viewholderquiz.fragments;
 
+import android.graphics.drawable.Drawable;
+
 /**
  * Created by whyaguilar on 1/14/16.
  */
 public class Item {
 
-    public int Id;
-    public String IconFile;
-    public String AnimeName;
-    public String DescMov;
+        public final Drawable icon;       // the drawable for the ListView item ImageView
+        public final String title;        // the text for the ListView item title
+        public final String description;  // the text for the ListView item description
 
-    public Item(int id, String iconFile, String animeName, String descMov) {
-
-        Id = id;
-        IconFile = iconFile;
-        AnimeName = animeName;
-        DescMov = descMov;
-
-
-    }
+        public Item(Drawable icon, String title, String description) {
+            this.icon = icon;
+            this.title = title;
+            this.description = description;
+        }
 }
+
--- app/src/main/java/sidespell/tech/viewholderquiz/fragments/ListViewFragment.java
+++ app/src/main/java/sidespell/tech/viewholderquiz/fragments/ListViewFragment.java
@@ -2,10 +2,16 @@ package sidespell.tech.viewholderquiz.fragments;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import sidespell.tech.viewholderquiz.R;
 
@@ -14,10 +20,13 @@ import sidespell.tech.viewholderquiz.R;
  */
 public class ListViewFragment extends Fragment {
 
+    private List<Item> movieItems;
 
     public static ListViewFragment newInstance() {
+
         return new ListViewFragment();
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,9 +35,34 @@ public class ListViewFragment extends Fragment {
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.anime, android.R.layout.simple_list_item_1);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
 
+        if (convertView == null) {
+            // inflate the GridView item layout
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.fragment_listview, parent, false);
+
+            viewHolder = new ViewHolder();
+
+            viewHolder.imgAnime = (ImageView) convertView.findViewById(R.id.imgAnime);
+            viewHolder.nameAnime = (TextView) convertView.findViewById(R.id.nameAnime);
+            viewHolder.descAnime = (TextView) convertView.findViewById(R.id.descAnime);
+            convertView.setTag(viewHolder);
+        }
+        else{
+            viewHolder = (ViewHolder) convertView.getTag();
+
+        }
+
+
+        return convertView;
     }
+
+    private static class ViewHolder {
+            ImageView imgAnime;
+            TextView nameAnime;
+            TextView descAnime;
+        }
+
 }
